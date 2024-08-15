155df03e715b9cabe253b5d605c927bee1004dc2
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-08-15 19:29:50 +0600

    add: footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
     return (
@@ -7,6 +8,10 @@ function App() {
             <Navbar />
             <div className="container">
                 <Outlet></Outlet>
+            </div>
+            <div className="bg-gray-900">
+
+                <Footer></Footer>
             </div>
         </div>
     );
