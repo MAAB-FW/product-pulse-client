bc3ffbb8ca59efd69bf08326810cab1b58e786ac
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-08-24 03:35:00 +0600

    update: caching added for data fetching

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,14 @@ import AuthProvider from "./routes/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            cacheTime: 1000 * 60 * 10, // Cache will persist for 10 minutes
+            refetchOnWindowFocus: false, // Disable refetching when window is focused
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
@@ -17,5 +24,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
                 <Toaster position="top-center" reverseOrder={false} />
             </QueryClientProvider>
         </AuthProvider>
-    </React.StrictMode>,
+    </React.StrictMode>
 );
